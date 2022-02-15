1fd2f8e177d3ed1f498974f500b643262fc0fcb5
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-02-16 00:53:37 +0100

    Finished project

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <img src="../images/troll-face.jpeg" alt="" className="header--image" />
+      <img src="../images/troll-face.png" alt="" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
